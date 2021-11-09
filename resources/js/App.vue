443a6bbe41318ea5821bf27f548f2610eec06a3c
Martijn Gussekloo <1426964+mgussekloo@users.noreply.github.com>
2021-11-09 10:16:12 +0100

    Worked some filters

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,37 +1,100 @@
 <template>
-	<div></div>
+	<div @click="nextSlide()"></div>
 </template>
 
 <script>
 	import anime from 'animejs/lib/anime.es.js';
-	import * as PIXI from 'pixi.js'
+	import * as PIXI from 'pixi.js';
+
+	import { PixelateFilter } from 'pixi-filters';
 
 	export default {
 		name: 'App',
+		data() {
+			return {
+				app: null,
+				img: null,
+				spriteIndex: 0,
+				spriteData: [
+					{ name: 'nook', 	url: 'static/nook.webp' },
+					{ name: 'blathers', url: 'static/blathers.webp' },
+					{ name: 'brewster', url: 'static/brewster.webp' }
+				],
+				resources: null,
+				pixelateFilter: null
+			}
+		},
+		methods: {
+			nextSlide() {
+				var tweenObj = { size: 1 };
+
+				anime({
+					targets: tweenObj,
+					duration: 1000,
+					size: [1, 100],
+					easing: 'linear',
+					update: ()=>{
+						this.pixelateFilter.size = tweenObj.size;
+					},
+				}).finished.then(() => {
+					console.log(this.spriteIndex);
+					return new Promise((resolve, reject) => {
+						this.spriteIndex++;
+						if (this.spriteIndex > 2) {
+							this.spriteIndex = 0;
+						}
+						this.img.texture = this.resources[ this.spriteData[ this.spriteIndex ].name ].texture;
+
+						resolve();
+					});
+				}).then(() => {
+					anime({
+						targets: tweenObj,
+						duration: 1000,
+						size: [100, 1],
+						easing: 'linear',
+						update: ()=>{
+							this.pixelateFilter.size = tweenObj.size;
+						},
+					})
+				});
+			}
+		},
 		mounted() {
-			const app = new PIXI.Application();
+			var app = new PIXI.Application();
 			app.renderer.resize(window.innerWidth, window.innerHeight);
 			this.$el.appendChild(app.view);
 
-			app.loader.add('tarot', 'static/tarot.jpg').load((loader, resources) => {
-			    const img = new PIXI.Sprite(resources.tarot.texture);
+			this.pixelateFilter = new PixelateFilter();
+			this.pixelateFilter.size = 1;
 
-			    // app.renderer.view.style.width = window.innerWidth;
-			    // app.renderer.view.style.height = window.innerHeight;
+			app.loader
+				.add(this.spriteData)
+				.load((loader, resources) => {
 
-			    img.x = app.renderer.width / 2;
-			    img.y = app.renderer.height / 2;
+				    const img = new PIXI.Sprite(resources[ this.spriteData[ this.spriteIndex ].name ].texture);
+				    img.filters = [ this.pixelateFilter ];
 
-			    img.anchor.x = 0.5;
-			    img.anchor.y = 0.5;
+				    // app.renderer.view.style.width = window.innerWidth;
+				    // app.renderer.view.style.height = window.innerHeight;
 
-			    app.stage.addChild(img);
+				    img.x = app.renderer.width / 2;
+				    img.y = app.renderer.height / 2;
 
-			    app.ticker.add(() => {
-			        img.rotation += 0.01;
-			    });
-			});
+				    img.anchor.x = 0.5;
+				    img.anchor.y = 0.5;
 
+				    app.stage.addChild(img);
+
+				    this.img = img;
+				    this.resources = resources;
+
+				    app.ticker.add(() => {
+				        img.rotation += 0.01;
+				    });
+				});
+
+			this.app = app;
 		}
 	}
 </script>
