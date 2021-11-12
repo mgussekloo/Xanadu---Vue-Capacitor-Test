d545afe36c002e3ab1326dc61472559e0e444f50
Martijn Gussekloo <1426964+mgussekloo@users.noreply.github.com>
2021-11-12 18:50:27 +0100

    Adding particle emitter on ios and web

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,19 +1,89 @@
 <template>
-	<div @click="nextSlide()"></div>
+	<div>
+		<div ref="canvas" @click="nextSlide" @touchstart="nextSlide()"></div>
+	</div>
 </template>
 
 <script>
 	import anime from 'animejs/lib/anime.es.js';
 	import * as PIXI from 'pixi.js';
+	import '@pixi/mixin-get-child-by-name';
 
 	import { PixelateFilter } from 'pixi-filters';
+	import * as particles from '@pixi/particle-emitter';
+
+	let offset = require('mouse-event-offset');
+
+	// var emitter = new PIXI.particles.Emitter(
+	let c = new PIXI.Container();
+	let pixelateFilter = new PixelateFilter();
+	pixelateFilter.size = 1;
+	c.filters = [ pixelateFilter ];
+
+	let pc = new PIXI.ParticleContainer();
+
+	let e = new particles.Emitter(pc, particles.upgradeConfig(
+{
+	"alpha": {
+		"start": 1,
+		"end": 0
+	},
+	"scale": {
+		"start": 0.5,
+		"end": 0.01,
+		"minimumScaleMultiplier": 1
+	},
+	"color": {
+		"start": "#e4f9ff",
+		"end": "#3fcbff"
+	},
+	"speed": {
+		"start": 200,
+		"end": 50,
+		"minimumSpeedMultiplier": 1
+	},
+	"acceleration": {
+		"x": 0,
+		"y": 0
+	},
+	"maxSpeed": 0,
+	"startRotation": {
+		"min": 0,
+		"max": 360
+	},
+	"noRotation": false,
+	"rotationSpeed": {
+		"min": 0,
+		"max": 0
+	},
+	"lifetime": {
+		"min": 0.2,
+		"max": 0.8
+	},
+	"blendMode": "normal",
+	"frequency": 0.001,
+	"emitterLifetime": 0.1,
+	"maxParticles": 500,
+	"pos": {
+		"x": 0,
+		"y": 0
+	},
+	"addAtBack": false,
+	"spawnType": "circle",
+	"spawnCircle": {
+		"x": 0,
+		"y": 0,
+		"r": 0
+	}
+}, ['static/Sparks.png']
+	));
+	e.emit = false;
 
 	export default {
 		name: 'App',
 		data() {
 			return {
 				app: null,
-				img: null,
 				spriteIndex: 0,
 				spriteData: [
 					{ name: 'nook', 	url: 'static/nook.webp' },
@@ -26,6 +96,10 @@
 		},
 		methods: {
 			nextSlide() {
+
+				// var pos = offset(event.changedTouches[0], this.$refs.canvas);
+				// alert(pos);
+
 				var tweenObj = { size: 1 };
 
 				anime({
@@ -34,7 +108,7 @@
 					size: [1, 100],
 					easing: 'linear',
 					update: ()=>{
-						this.pixelateFilter.size = tweenObj.size;
+						pixelateFilter.size = tweenObj.size;
 					},
 				}).finished.then(() => {
 					console.log(this.spriteIndex);
@@ -43,7 +117,7 @@
 						if (this.spriteIndex > 2) {
 							this.spriteIndex = 0;
 						}
-						this.img.texture = this.resources[ this.spriteData[ this.spriteIndex ].name ].texture;
+						c.getChildByName("Sprite").texture = this.resources[ this.spriteData[ this.spriteIndex ].name ].texture;
 
 						resolve();
 					});
@@ -54,39 +128,49 @@
 						size: [100, 1],
 						easing: 'linear',
 						update: ()=>{
-							this.pixelateFilter.size = tweenObj.size;
+							pixelateFilter.size = tweenObj.size;
 						},
 					})
+
+				});
+			},
+			clickCanvas(event) {
+				var event = event.type.includes('touch') ? event.targetTouches[0] : event;
+				var p = offset(event, this.$refs.canvas);
+
+				e.resetPositionTracking();
+				e.updateOwnerPos(p[0], p[1]);
+				e.playOnce(() => {
 				});
 			}
 		},
 		mounted() {
+ 			this.$refs.canvas.addEventListener('mousedown', this.clickCanvas, false);
+    		this.$refs.canvas.addEventListener('touchstart', this.clickCanvas, false);
+
 			var app = new PIXI.Application();
 			app.renderer.resize(window.innerWidth, window.innerHeight);
-			this.$el.appendChild(app.view);
-
-			this.pixelateFilter = new PixelateFilter();
-			this.pixelateFilter.size = 1;
+			this.$refs.canvas.appendChild(app.view);
 
 			app.loader
 				.add(this.spriteData)
 				.load((loader, resources) => {
-
 				    const img = new PIXI.Sprite(resources[ this.spriteData[ this.spriteIndex ].name ].texture);
-				    img.filters = [ this.pixelateFilter ];
 
 				    // app.renderer.view.style.width = window.innerWidth;
 				    // app.renderer.view.style.height = window.innerHeight;
 
+				    img.name = "Sprite";
 				    img.x = app.renderer.width / 2;
 				    img.y = app.renderer.height / 2;
 
 				    img.anchor.x = 0.5;
 				    img.anchor.y = 0.5;
 
-				    app.stage.addChild(img);
+				    app.stage.addChild(c);
+				    c.addChild(img);
+				    app.stage.addChild(pc);
 
-				    this.img = img;
 				    this.resources = resources;
 
 				    app.ticker.add(() => {
@@ -94,7 +178,15 @@
 				    });
 				});
 
+			app.ticker.add((delta) => {
+				e.update( delta / PIXI.settings.TARGET_FPMS / 1000 );
+			});
+
 			this.app = app;
+		},
+		beforeUnmount() {
+		    this.$refs.canvas.removeEventListener('mousedown', this.clickCanvas, false);
+		    this.$refs.canvas.removeEventListener('touchstart', this.clickCanvas, false);
 		}
 	}
 </script>
